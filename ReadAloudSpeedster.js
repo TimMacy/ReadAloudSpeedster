ec52593fd41bcd0d82a36d6918aff2e745dc857c
Tim Macy <174056878+TimMacy@users.noreply.github.com>
2025-12-14 14:09:23 +0800

    Update ReadAloudSpeedster.js
    adjustments for the latest DOM changes

--- ReadAloudSpeedster.js
+++ ReadAloudSpeedster.js
@@ -3,7 +3,7 @@
 // @description  Set playback speed for Read Aloud on ChatGPT.com, navigate between messages, and open a settings menu by clicking the speed display to toggle additional UI tweaks. Features include color-coded icons under ChatGPT's responses, highlighted color for bold text, compact sidebar, square design, and more.
 // @author       Tim Macy
 // @license      AGPL-3.0-or-later
-// @version      5.12
+// @version      5.12.1
 // @namespace    TimMacy.ReadAloudSpeedster
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com
 // @match        https://*.chatgpt.com/*
@@ -20,7 +20,7 @@
 *                                                                       *
 *                    Copyright © 2025 Tim Macy                          *
 *                    GNU Affero General Public License v3.0             *
-*                    Version: 5.12 - Read Aloud Speedster               *
+*                    Version: 5.12.1 - Read Aloud Speedster             *
 *                                                                       *
 *             Visit: https://github.com/TimMacy                         *
 *                                                                       *
@@ -292,7 +292,8 @@
             padding: 0;
         }
 
-        #thread-bottom-container {
+        #thread-bottom-container,
+        #thread-bottom-container div.mb-4.flex-1 {
             margin-bottom: 0;
         }
 
