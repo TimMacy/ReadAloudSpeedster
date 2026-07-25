474e02c23976fb555608e86d9ebf3a30a5c9b4b5
Tim Macy <174056878+TimMacy@users.noreply.github.com>
2026-07-25 12:00:25 +0800

    Update ReadAloudSpeedster.js
    v5.32.3 adjustments for DOM changes

--- ReadAloudSpeedster.js
+++ ReadAloudSpeedster.js
@@ -3,7 +3,7 @@
 // @description  Set playback speed for Read Aloud on ChatGPT.com, navigate between messages, and open a settings menu by clicking the speed display to toggle additional UI tweaks. Features include color-coded icons under ChatGPT's responses, highlighted color for bold text, compact sidebar, square design, and more.
 // @author       Tim Macy
 // @license      AGPL-3.0-or-later
-// @version      5.32.2
+// @version      5.32.3
 // @namespace    TimMacy.ReadAloudSpeedster
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com
 // @match        https://chatgpt.com/*
@@ -21,7 +21,7 @@
 *                                                                       *
 *                    Copyright © 2026 Tim Macy                          *
 *                    GNU Affero General Public License v3.0             *
-*                    Version: 5.32.2 - Read Aloud Speedster             *
+*                    Version: 5.32.3 - Read Aloud Speedster             *
 *                                                                       *
 *             Visit: https://github.com/TimMacy                         *
 *                                                                       *
@@ -912,10 +912,6 @@
             margin-right: unset;
         }
 
-        [data-composer-transition-slot="trailing"]:has(> div:nth-child(2) > div > span) #CentAnni-gpt-model-quickbar {
-            display: none;
-        }
-
         .CentAnni-gpt-model-btn.CentAnni-active {
             border-color: rgb(1, 105, 204);
 
@@ -974,8 +970,13 @@
         html:has(main header div.gap-4.ps-4) #CentAnni-gpt-model-quickbar,
         html:has(#main > div > header > div:nth-child(1) > h1) #CentAnni-gpt-model-quickbar,
         html:has(.bg-token-bg-primary.absolute.start-0.z-20.h-full.overflow-hidden) #CentAnni-gpt-model-quickbar,
+        #thread-bottom [data-composer-transition-slot="trailing"]:has(span[data-animated-slider-trigger="true"]) #CentAnni-gpt-model-quickbar,
         html:has(nav > aside > a.__menu-item:not(:disabled):not([data-disabled])[data-active] svg use[href*="#266724"]) #CentAnni-gpt-model-quickbar {
             display: none;
+        }
+
+        #thread-bottom form div.min-h-0 {
+            min-height: unset !important;
         }
 
         @media (max-width: 768px) {
